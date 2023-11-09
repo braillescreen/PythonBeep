bda3bab76f897a530ac4e01214f246f72dbf2588
Patrick Wilson <patrick@pwry.net>
2023-11-08 21:17:33 -0600

    Don't claim you are something you aren't! Plus other improvements; not actually worth using still though.

--- BeepPython.py
+++ BeepPython.py
@@ -1,10 +1,3 @@
-"""
-Beep Python
-Make your program beep!
-Copyright (c) 2021 Patrick Wilson
-
-Attempts to beep in two different ways for now, but more will exist later.
-"""
 import winsound, time
 print("Okay, atempting to print the terminal bell.")
 input()
@@ -13,7 +6,7 @@ print("Now doing a winsound beep.")
 input()
 winsound.Beep(1000,1000)
 print("Okay, now doing a thing where we go through a loop and beep.")
-for x in range(2000):
+
+for x in range(37, 2000, 49):
 	if x<37: continue
 	winsound.Beep(x,200)
-	x=x+49
